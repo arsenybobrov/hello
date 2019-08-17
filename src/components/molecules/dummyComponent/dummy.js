7962e9b8dd030d075e04159b9189e40e01a1ad7d
Arseny Bobrov <info@abobrov.com>
2019-08-17 17:44:04 +0200

    feat: remove dummies

--- src/components/molecules/dummyComponent/dummy.js
+++
@@ -1,8 +0,0 @@
-import getBreakpoint from '../../../helpers/js/getBreakpoint.helper';
-
-const dummy = (prop) => {
-  console.log(prop);
-  console.log('breakpoint is: ', getBreakpoint(window.innerWidth));
-};
-
-export default dummy;
--- src/config/index.js
+++ src/config/index.js
@@ -2,19 +2,12 @@ import debounce from 'lodash/debounce';
 import './main.scss';
 import '../assets/fonts/exo2-regular-webfont.woff';
 import '../assets/fonts/exo2-regular-webfont.woff2';
-
-import dummy from '../components/molecules/dummyComponent/dummy';
-import anotherDummy from '../components/atoms/anotherDummyComponent/anotherDummy';
 import getBreakpoint from '../helpers/js/getBreakpoint.helper';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('document is ready!');
-
-  dummy('dummy is rocking');
-  anotherDummy();
 });
 
 window.addEventListener('resize', debounce(() => {
-  console.log('document resized!');
   console.log('breakpoint is: ', getBreakpoint(window.innerWidth));
 }, 250));
